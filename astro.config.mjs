2b30f47938397548ac22bb9139c4082e32f76644
thermo-lab <fagundo@gmail.com>
2026-02-18 18:09:25 -0600

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
-
-import tailwindcss from '@tailwindcss/vite';
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()]
-  }
+  // 1. The full URL to your site (optional but good practice)
+  site: 'https://thermo-lab.github.io',
+  
+  // 2. The crucial part: Your repo name with a leading slash
+  base: '/strange-attractor-website', 
+  
+  integrations: [tailwind()]
 });

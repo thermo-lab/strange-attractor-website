0dcdf3f77e989bc51d6a1ee93bded14389c2e67f
thermo-lab <fagundo@gmail.com>
2026-02-22 23:03:43 -0600

    Update config.ts

--- src/content/config.ts
+++ src/content/config.ts
@@ -5,6 +5,7 @@ const printsCollection = defineCollection({
   schema: z.object({
     title: z.string(),
     peechoId: z.string(),       // The ID required for the buy button
+    galleryId: z.string(),       // The ID required to load the attractor viewer
     coverImage: z.string(),     // Path to image in /public/images/
     date: z.date(),             // Used for sorting
     featured: z.boolean().default(false).optional(),
